4ab83c1ba44094a86632d7e67f3a6f8c516490cb
Moritz Kiefer <moritz.kiefer@gmail.com>
2013-02-21 22:32:26 +0100

    Add setup.py and change paths

---
+++ pytranscode/__init__.py
@@ -0,0 +1,5 @@
+import pytranscode.ffmpeg
+import pytranscode.presets
+import pytranscode.runner
+import pytranscode.splash
+import pytranscode.video_info
---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup, find_packages
+setup(name='pytranscode',
+        author='Martin P. Buhr',
+        packages=find_packages())
